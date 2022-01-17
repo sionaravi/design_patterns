5a1683b16e4c7d84f51fe1109cb22238ec8efc35
Siona Ravi <70077939+sionaravi@users.noreply.github.com>
2022-01-17 00:42:31 -0500

    Update BubbleSort.java

--- BubbleSort.java
+++ BubbleSort.java
@@ -4,14 +4,13 @@ package strategy_design;
 	import java.util.Collections;
 
 	/*
-	 *  
 	 * used this video for code https://youtu.be/467BD-yuvmk
 	 */
 	public class BubbleSort implements SortBehavior {
 		
 		public ArrayList<String> sort(ArrayList<String> data) {
 			
-			/*for (int j = 0; j < data.size() - 1; j++ ) {
+			for (int j = 0; j < data.size() - 1; j++ ) {
 				
 				for (int i = 0; i < data.size()-1; i++) {
 					
@@ -21,12 +20,9 @@ package strategy_design;
 						data.set(i, data.get(i+1));
 						data.set(i+1, temp); */
 						Collections.sort(data);
-				        System.out.println(data);
-					
-				
-			
-			return data;
-		}
-	}
+				                System.out.println(data);
+					}
+					return data;
+				}
 
 
