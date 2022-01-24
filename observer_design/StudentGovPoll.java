897c49a8d9adf0846d35164809ea713b84e41e60
Siona Ravi <70077939+sionaravi@users.noreply.github.com>
2022-01-23 21:18:22 -0500

    Update StudentGovPoll.java

--- observer_design/StudentGovPoll.java
+++ observer_design/StudentGovPoll.java
@@ -2,7 +2,7 @@ package observer_design;
 
 /**
  * Class that creates the S and its observers
- * Siona Ravi
+ * @author Siona Ravi
  */
 
 import java.util.ArrayList;
@@ -15,52 +15,60 @@ public class StudentGovPoll implements Subject {
 	private String school;
 	private int numUpdates;
 	
+	/**
+     	* Conducts a poll for the school
+     	*/
 	public StudentGovPoll(String school) {
 		this.school = school;
 		observers = new ArrayList<Observer>();
 	}
 	
-	
-
+	/**
+	 * Registers the observer in the list.
+	 */
 	@Override
 	public void registerObserver(Observer observer) {
 		observers.add(observer);
 	}
+	
+	/**
+	 *  Removes the observer in the list.
+	 */
 	@Override
 	public void removeObserver(Observer observer) {
 		 observers.remove(observer);
 	}
+	
+	/**
+	 * Notifies the observer in the list.
+	 */
 	@Override
 	public void notifyObservers() {
 		for (Observer observer : observers) {
 			observer.update(votes);
-		}
+	}
+	
+	/**
+    	 * Takes the president names
+     	*/
+	public void addCandidate(String president) {
 		
 	}
 	
-	public void addCandidate(String president) {
+	/**
+    	 * Takes the votes and then calls to notify the obeservers
+    	 */
+	public void enterVotes(String president, int num) {
 		this.votes = votes;
 		notifyObservers();
-		
-		
 	}
 	
-	public void enterVotes(String president, int num) {
-		
-		
-	}
-	
+	/**
+	 *  Returns the name of the school
+	 */
 	public String getSchool() {
 		return this.school;
 		
 	}
-
-	
-
-	
-	
-	
-	
-	
 
 }
