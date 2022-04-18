5a14ed7bb1f9e6661ed38527568aa8c013c0ea67
Siona Ravi <70077939+sionaravi@users.noreply.github.com>
2022-04-17 22:05:43 -0400

    Add files via upload

---
+++ state_design/state_design/State.java
@@ -0,0 +1,19 @@
+package state_design;
+
+/**
+ * State Interface
+ * @author sionaravi
+ */
+
+public interface State {
+	
+	public void pressStarButton();
+	
+	public void pressHappyButton();
+	
+	public void pressEnglishButton();
+	
+	public void pressFrenchButton();
+	
+	public void pressSpanishButton();
+}
